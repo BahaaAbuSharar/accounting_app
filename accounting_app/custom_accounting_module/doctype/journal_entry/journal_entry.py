8726501b7112a52b2bacf30f667f451519904eb2
Bahaa Abu Sharar <baha13667@gmail.com>
2025-06-28 13:12:50 +0300

    feat: validate journal entry balance and raise error if not balanced

--- accounting_app/custom_accounting_module/doctype/journal_entry/journal_entry.py
+++ accounting_app/custom_accounting_module/doctype/journal_entry/journal_entry.py
@@ -1,11 +1,20 @@
 import frappe
 from frappe import _
+from frappe.model.document import Document
+
 
 class JournalEntry(Document):
     def validate(self):
         self.calculate_totals()
+        self.check_balance()
 
     def calculate_totals(self):
         self.total_debit = sum([d.debit or 0 for d in self.accounting_entries])
         self.total_credit = sum([d.credit or 0 for d in self.accounting_entries])
         self.difference = self.total_debit - self.total_credit
+
+    def check_balance(self):
+        if round(self.difference, 2) != 0:
+            frappe.throw(_("القيد غير متوازن: مجموع المدين ({0}) لا يساوي مجموع الدائن ({1})").format(
+                self.total_debit, self.total_credit
+            ))
