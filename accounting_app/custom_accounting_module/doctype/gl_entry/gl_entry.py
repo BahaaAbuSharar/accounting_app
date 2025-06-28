d5f8a8e9b938c00ea4c443470d78811b0e987aa2
Bahaa Abu Sharar <baha13667@gmail.com>
2025-06-28 16:19:21 +0300

    Add validation to prevent setting both debit and credit amounts simultaneously

--- accounting_app/custom_accounting_module/doctype/gl_entry/gl_entry.py
+++ accounting_app/custom_accounting_module/doctype/gl_entry/gl_entry.py
@@ -1,8 +1,9 @@
-# Copyright (c) 2025, Bahaa and contributors
-# For license information, please see license.txt
-
-# import frappe
 from frappe.model.document import Document
+import frappe
 
 class GLEntry(Document):
-	pass
+
+    def validate(self):
+        # مثال: منع تعيين قيمتين في نفس الوقت
+        if self.debit_amount and self.credit_amount:
+            frappe.throw("You cannot have both Debit and Credit values at the same time.")
