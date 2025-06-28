fadf8d1954adb8875664b9daa046c4ab24dbcfaf
Bahaa Abu Sharar <baha13667@gmail.com>
2025-06-28 11:45:18 +0300

    Reset source and destination accounts on payment_type change in Payment Entry

--- accounting_app/custom_accounting_module/doctype/payment_entry/payment_entry.js
+++ accounting_app/custom_accounting_module/doctype/payment_entry/payment_entry.js
@@ -5,4 +5,10 @@ frappe.ui.form.on('Payment Entry', {
             frm.set_value('posting_date', frappe.datetime.get_today());
         }
     },
+
+    payment_type: function(frm) {
+        // إعادة ضبط الحسابات عند تغيير نوع الدفع
+        frm.set_value("account_paid_from", null);
+        frm.set_value("account_paid_to", null);
+    },
 });
