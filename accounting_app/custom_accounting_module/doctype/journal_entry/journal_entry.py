b9e000d67e90b1189010804a20fe951f985c6975
Bahaa Abu Sharar <baha13667@gmail.com>
2025-06-30 12:58:39 +0300

    feat: implement GL entries and validations for Journal Entry

--- accounting_app/custom_accounting_module/doctype/journal_entry/journal_entry.py
+++ accounting_app/custom_accounting_module/doctype/journal_entry/journal_entry.py
@@ -18,3 +18,38 @@ class JournalEntry(Document):
             frappe.throw(_("The restriction is unbalanced: the total debtor ({0}) is not equal to the total creditor ({1})").format(
                 self.total_debit, self.total_credit
             ))
+    def on_submit(self):
+        self.make_gl_entries()
+
+    def on_cancel(self):
+        self.make_gl_entries_reverse()
+
+    def make_gl_entries(self):
+        for row in self.accounting_entries:
+            frappe.get_doc({
+                "doctype": "GL Entry",
+                "posting_date": self.posting_date,
+                "party": row.party,
+                "account": row.account,
+                "debit_amount": row.debit or 0,
+                "credit_amount": row.credit or 0,
+                "voucher_type": "Journal Entry",
+                "voucher_number": self.name,
+                "is_cancelled": 0,
+                "remarks": row.description
+            }).insert()
+
+    def make_gl_entries_reverse(self):
+        for row in self.accounting_entries:
+            frappe.get_doc({
+                "doctype": "GL Entry",
+                "posting_date": self.posting_date,
+                "party": row.party,
+                "account": row.account,
+                "debit_amount": row.credit or 0,
+                "credit_amount": row.debit or 0,
+                "voucher_type": "Journal Entry",
+                "voucher_number": self.name,
+                "is_cancelled": 1,
+                "remarks": row.description
+            }).insert()
