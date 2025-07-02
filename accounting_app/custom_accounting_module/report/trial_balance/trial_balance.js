8ca7f2e5d99593c507a2055c1ed7b68566f56632
Bahaa Abu Sharar <baha13667@gmail.com>
2025-07-02 15:49:41 +0300

    refactor: remove default date filters from

--- accounting_app/custom_accounting_module/report/trial_balance/trial_balance.js
+++ accounting_app/custom_accounting_module/report/trial_balance/trial_balance.js
@@ -4,13 +4,11 @@ frappe.query_reports["Trial Balance"] = {
             fieldname: "from_date",
             label: __("From Date"),
             fieldtype: "Date",
-            default: frappe.sys_defaults.year_start
         },
         {
             fieldname: "to_date",
             label: __("To Date"),
             fieldtype: "Date",
-            default: frappe.sys_defaults.year_end
         },
         {
             fieldname: "account",
